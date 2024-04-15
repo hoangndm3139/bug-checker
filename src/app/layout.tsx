6338798fe51d509a541da2bb061f6d80c0a1cf4f
HoangNDM6 <mhoang.9699@gmail.com>
2024-04-15 13:41:25 +0700

    init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import React, { ReactNode } from 'react';
 import AppWrappers from './AppWrappers';
+import { Metadata } from 'next';
 // import '@asseinfo/react-kanban/dist/styles.css';
 // import '/public/styles/Plugins.css';
+
+export const metadata: Metadata = {
+  title: 'Bug Checker',
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
