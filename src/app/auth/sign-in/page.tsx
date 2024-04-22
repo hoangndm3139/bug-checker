b81b0c7ba004834f671df45bc3c2f0643ba1d2ae
hoangndm <mhoang.9699@gmail.com>
2024-04-22 22:04:41 +0700

    init commit

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -12,7 +12,7 @@ function SignInDefault() {
           {/* Sign in section */}
           <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
             <h3 className="mb-2.5 text-4xl font-bold text-navy-700 dark:text-white">
-              Sign In
+              {/* Sign In */}
             </h3>
             <p className="mb-9 ml-1 text-base text-gray-600">
               Enter your email and password to sign in!
